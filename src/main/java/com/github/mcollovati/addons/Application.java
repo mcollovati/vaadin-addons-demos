517d9c95dedb78be9e3e8f4037e2a4e770801b73
start.vaadin.com <no-reply@vaadin.com>
2022-11-22 22:46:21 +0000

    Generated project

---
+++ src/main/java/com/github/mcollovati/addons/Application.java
@@ -0,0 +1,27 @@
+package com.github.mcollovati.addons;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "add-ondemos")
+@PWA(name = "Add-on Demos", shortName = "Add-on Demos", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
