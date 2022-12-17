0aea3f8db74428a16e7015ecb713886bdc32a493
Marco Collovati <mcollovati@gmail.com>
2022-12-17 15:58:30 +0100

    fix: fixed demo routes

--- src/main/java/com/github/mcollovati/addons/views/twitterwidgets/TwitterWidgetsAddon.java
+++ src/main/java/com/github/mcollovati/addons/views/twitterwidgets/TwitterWidgetsAddon.java
@@ -2,15 +2,14 @@ package com.github.mcollovati.addons.views.twitterwidgets;
 
 import com.github.mcollovati.addons.model.Addon;
 
-import com.vaadin.flow.component.Component;
-
 public class TwitterWidgetsAddon extends Addon {
 
     public static final String DESCRIPTION = "<a href=\"https://vaadin.com\">Vaadin</a> Flow component to embed <a href=\"https://dev.twitter.com/web/overview\">Twitter</a> widgets to your application.";
 
     public TwitterWidgetsAddon() {
         super("Twitter widgets for Vaadin", DESCRIPTION,
-                "twitter-widgets-demo.png", TimelineDemo.class,
+                "twitter-widgets-demo.png",
+                TimelineDemo.class,
                 "https://github.com/mcollovati/vaadin-twitter-widgets");
     }
 }
