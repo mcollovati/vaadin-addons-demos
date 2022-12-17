a577ce5d5a2ebd3192bdcb2fde4a59033d589347
Marco Collovati <mcollovati@gmail.com>
2022-12-17 16:10:41 +0100

    test: error commit

--- src/main/java/com/github/mcollovati/addons/views/twitterwidgets/TwitterWidgetsAddon.java
+++ src/main/java/com/github/mcollovati/addons/views/twitterwidgets/TwitterWidgetsAddon.java
@@ -6,6 +6,7 @@ public class TwitterWidgetsAddon extends Addon {
 
     public static final String DESCRIPTION = "<a href=\"https://vaadin.com\">Vaadin</a> Flow component to embed <a href=\"https://dev.twitter.com/web/overview\">Twitter</a> widgets to your application.";
 
+    sdfdfs
     public TwitterWidgetsAddon() {
         super("Twitter widgets for Vaadin", DESCRIPTION,
                 "twitter-widgets-demo.png",
